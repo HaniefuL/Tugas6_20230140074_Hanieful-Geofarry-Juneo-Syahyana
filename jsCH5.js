49e2a4b58a568223cb0da41bc0d3f9f2048893ba
HaniefuL <haniefulgeofarry@gmail.com>
2025-04-30 20:37:55 +0700

    first commit

---
+++ jsCH5.js
@@ -0,0 +1,11 @@
+video.addEventListener("click", function video() {
+    alert("ini video LIVERPOOL");
+});
+
+audio.addEventListener("click", function audio() {
+    alert("ini Audio LIVERPOOL");
+});
+
+gambar.addEventListener("click", function gambar() {
+    alert("ini gambar LIVERPOOL");
+});
